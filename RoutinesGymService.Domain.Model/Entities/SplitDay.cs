04f39abb3a883c7cdd0d704d30fcd8a6a9061600
marcos.remon <marcos.remon@kintech-engineering.com>
2025-07-25 12:09:54 +0200

    entidades creadas

---
+++ RoutinesGymService.Domain.Model/Entities/SplitDay.cs
@@ -0,0 +1,32 @@
+﻿using RoutinesGymService.Domain.Model.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoutinesGymService.Domain.Model.Entities
+{
+    [Table("split_days")]
+    public class SplitDay
+    {
+        [Key]
+        [Column("split_day_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SplitDayId { get; set; }
+
+        [Column("day_name")]
+        public WeekDay? DayName { get; set; } 
+
+        [Column("routine_id")]
+        public int RoutineId { get; set; }
+
+        [Column("day_exercises_description")]
+        public string DayExercisesDescription { get; set; } = string.Empty;
+
+
+        [ForeignKey("RoutineId")]
+        [InverseProperty("SplitDays")]
+        public virtual Routine? Routine { get; set; }
+
+        [InverseProperty("SplitDay")]
+        public virtual ICollection<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
