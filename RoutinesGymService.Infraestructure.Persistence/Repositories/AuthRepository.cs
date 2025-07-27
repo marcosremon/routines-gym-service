211c45c63f66c4d22dc4991eb55d725ecf757094
Marcos <mremonb@iesch.org>
2025-07-27 13:54:53 +0200

    context añadido a los repositorys y async

--- RoutinesGymService.Infraestructure.Persistence/Repositories/AuthRepository.cs
+++ RoutinesGymService.Infraestructure.Persistence/Repositories/AuthRepository.cs
@@ -1,11 +1,19 @@
 ﻿using RoutinesGymService.Application.DataTransferObject.Interchange.Auth.Login;
 using RoutinesGymService.Application.Interface.Repository;
+using RoutinesGymService.Infraestructure.Persistence.Context;
 
 namespace RoutinesGymService.Infraestructure.Persistence.Repositories
 {
     public class AuthRepository : IAuthRepository
     {
-        public Task<LoginResponse> Login(LoginRequest loginRequest)
+        private readonly ApplicationDbContext _context;
+
+        public AuthRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<LoginResponse> Login(LoginRequest loginRequest)
         {
             throw new NotImplementedException();
         }
