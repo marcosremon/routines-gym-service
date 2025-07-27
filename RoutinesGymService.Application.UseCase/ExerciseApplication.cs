ce6715bce621afe565ee444799f1258eab6320c8
Marcos <mremonb@iesch.org>
2025-07-27 12:47:29 +0200

    applications hechos

--- RoutinesGymService.Application.UseCase/ExerciseApplication.cs
+++ RoutinesGymService.Application.UseCase/ExerciseApplication.cs
@@ -1,6 +1,45 @@
-﻿namespace RoutinesGymService.Application.UseCase
+﻿using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.AddExercise;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.AddExerciseProgress;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.DeleteExercise;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.GetExercisesByDayAndRoutineId;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.UpdateExercise;
+using RoutinesGymService.Application.Interface.Application;
+using RoutinesGymService.Application.Interface.Repository;
+
+namespace RoutinesGymService.Application.UseCase
 {
-    public class ExerciseApplication
+    public class ExerciseApplication : IExerciseApplication
     {
+        private readonly IExerciseRepository _exerciseRepository;
+
+        public ExerciseApplication(IExerciseRepository exerciseRepository)
+        {
+            _exerciseRepository = exerciseRepository;
+        }
+
+        public async Task<AddExerciseResponse> AddExercise(AddExerciseRequest addExerciseRequest)
+        {
+            return await _exerciseRepository.addExercise(addExerciseRequest);
+        }
+
+        public async Task<AddExerciseAddExerciseProgressResponse> AddExerciseProgress(AddExerciseAddExerciseProgressRequest addExerciseRequest)
+        {
+            return await _exerciseRepository.AddExerciseProgress(addExerciseRequest);
+        }
+
+        public async Task<DeleteExerciseResponse> DeleteExercise(DeleteExerciseRequest deleteExerciseRequest)
+        {
+            return await _exerciseRepository.DeleteExercise(deleteExerciseRequest);
+        }
+
+        public async Task<GetExercisesByDayAndRoutineIdResponse> GetExercisesByDayAndRoutineId(GetExercisesByDayAndRoutineIdRequest getExercisesByDayNameRequest)
+        {
+            return await _exerciseRepository.GetExercisesByDayAndRoutineId(getExercisesByDayNameRequest);
+        }
+
+        public async Task<UpdateExerciseResponse> UpdateExercise(UpdateExerciseRequest updateExerciseRequest)
+        {
+            return await _exerciseRepository.UpdateExercise(updateExerciseRequest);
+        }
     }
 }
