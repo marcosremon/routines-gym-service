d1f0da8caf08d8b2c6c106acafef2245a6ebe9af
Marcos <mremonb@iesch.org>
2025-07-27 13:47:44 +0200

    dependency injection hecho y programs

--- RoutinesGymService/Program.cs
+++ RoutinesGymService/Program.cs
@@ -1,4 +1,12 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+using RoutinesGymService.Infraestructure.Persistence.Context;
+using RoutinesGymService.Infraestructure.Persistence.Dependencies;
+using RoutinesGymService.Transversal.Mailing;
+using RoutinesGymService.Transversal.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +16,53 @@ builder.Services.AddControllers(options =>
     options.Conventions.Insert(0, new RoutePrefixConvention("api"));
 });
 
+// Configuración mínima
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuración de Mails
+MailUtils.Initialize(builder.Configuration);
+
+// Configuración de PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
+
+// Registrar servicios de infraestructura
+builder.Services.AddInfrastructureServices();
+
+// Configuración JWT
+var jwtSettings = builder.Configuration.GetSection("JWT");
+var keyString = jwtSettings["Key"] ?? throw new InvalidOperationException("JWT Key no está configurado");
+Console.WriteLine($"JWT Key: {keyString}"); // Debug
+var key = Encoding.ASCII.GetBytes(keyString);
+JwtUtils.Initialize(builder.Configuration);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,8 +71,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// Habilitar CORS (¡Importante que esté antes de UseAuthentication/UseAuthorization!)
+app.UseCors("AllowSpecificOrigin");
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
