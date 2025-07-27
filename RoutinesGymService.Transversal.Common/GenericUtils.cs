1d859885c1e81a9e545e8dc633ce3371cf2b5f40
Marcos <mremonb@iesch.org>
2025-07-27 14:04:52 +0200

    generic utils añadido

---
+++ RoutinesGymService.Transversal.Common/GenericUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace RoutinesGymService.Transversal.Common
+{
+    public class GenericUtils
+    {
+        public static string ChangeDayLanguage(string dayName)
+        {
+            switch (dayName)
+            {
+                case "Lunes": dayName = "Monday"; break;
+                case "Martes": dayName = "Tuesday"; break;
+                case "Miércoles": dayName = "Wednesday"; break;
+                case "Jueves": dayName = "Thursday"; break;
+                case "Viernes": dayName = "Friday"; break;
+                case "Sábado": dayName = "Saturday"; break;
+                case "Domingo": dayName = "Sunday"; break;
+                default: dayName = "Day"; break;
+            }
+
+            return dayName;
+        }
+    }
+}
