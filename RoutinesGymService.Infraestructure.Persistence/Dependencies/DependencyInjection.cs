30444cfdcc78f0ed89bed578fda13a80f3ee96bc
Marcos <mremonb@iesch.org>
2025-07-27 13:47:44 +0200

    dependency injection hecho y programs

--- RoutinesGymService.Infraestructure.Persistence/Dependencies/DependencyInjection.cs
+++ RoutinesGymService.Infraestructure.Persistence/Dependencies/DependencyInjection.cs
@@ -1,6 +1,32 @@
-﻿namespace RoutinesGymService.Infraestructure.Persistence.Dependencies
+﻿using Microsoft.Extensions.DependencyInjection;
+using RoutinesGymService.Application.Interface.Application;
+using RoutinesGymService.Application.Interface.Repository;
+using RoutinesGymService.Application.UseCase;
+using RoutinesGymService.Infraestructure.Persistence.Repositories;
+
+namespace RoutinesGymService.Infraestructure.Persistence.Dependencies
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            // Application
+            services.AddScoped<IUserApplication, UserApplication>();
+            services.AddScoped<IRoutineApplication, RoutineApplication>();
+            services.AddScoped<ISplitDayApplication, SplitDayApplication>();
+            services.AddScoped<IExerciseApplication, ExerciseApplication>();
+            services.AddScoped<IAuthApplication, AuthApplication>();
+            services.AddScoped<IFriendApplication, FriendApplication>();
+
+            // Repository
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoutineRepository, RoutineRepository>();
+            services.AddScoped<ISplitDayRepository, SplitDayRepository>();
+            services.AddScoped<IExerciseRepository, ExerciseRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IFriendRepository, FriendRepository>();
+
+            return services;
+        }
     }
 }
--- RoutinesGymService.Infraestructure.Persistence/Repositories/UserRepository.cs
+++ RoutinesGymService.Infraestructure.Persistence/Repositories/UserRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace RoutinesGymService.Infraestructure.Persistence.Repositories
+﻿using RoutinesGymService.Application.DataTransferObject.Interchange.User.Create.ChangePasswordWithPasswordAndEmail;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Create.CreateGenericUser;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Create.CreateGoogleUser;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Create.CreateNewPassword;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Create.CreateUser;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.DeleteUser;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Get.GetUserByEmail;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.Get.GetUsers;
+using RoutinesGymService.Application.DataTransferObject.Interchange.User.UpdateUser;
+using RoutinesGymService.Application.Interface.Repository;
+
+namespace RoutinesGymService.Infraestructure.Persistence.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        public Task<ChangePasswordWithPasswordAndEmailResponse> ChangePasswordWithPasswordAndEmail(ChangePasswordWithPasswordAndEmailRequest changePasswordWithPasswordAndEmailRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateGoogleUserResponse> CreateGoogleUser(CreateGenericUserRequest createGenericUserRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateNewPasswordResponse> CreateNewPassword(CreateNewPasswordRequest createNewPasswordRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateUserResponse> CreateUser(CreateGenericUserRequest createGenericUserRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteUserResponse> DeleteUser(DeleteUserRequest deleteUserRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserByEmailResponse> GetUserByEmail(GetUserByEmailRequest getUserByEmailRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUsersResponse> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateUserResponse> UpdateUser(UpdateUserRequest updateUserRequest)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
