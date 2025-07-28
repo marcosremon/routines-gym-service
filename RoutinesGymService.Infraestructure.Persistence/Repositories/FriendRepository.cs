95975f95e0c77f13a335aa971951814b2c61dc25
marcos.remon <marcos.remon@kintech-engineering.com>
2025-07-28 16:54:49 +0200

    AddNewUserFriend hecho

--- RoutinesGymService.Infraestructure.Persistence/Repositories/FriendRepository.cs
+++ RoutinesGymService.Infraestructure.Persistence/Repositories/FriendRepository.cs
@@ -1,7 +1,9 @@
-﻿using RoutinesGymService.Application.DataTransferObject.Interchange.Friend.AddNewUserFriend;
+﻿using Microsoft.EntityFrameworkCore;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Friend.AddNewUserFriend;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Friend.DeleteFriend;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Friend.GetAllUserFriends;
 using RoutinesGymService.Application.Interface.Repository;
+using RoutinesGymService.Domain.Model.Entities;
 using RoutinesGymService.Infraestructure.Persistence.Context;
 
 namespace RoutinesGymService.Infraestructure.Persistence.Repositories
@@ -17,7 +19,55 @@ namespace RoutinesGymService.Infraestructure.Persistence.Repositories
 
         public async Task<AddNewUserFriendResponse> AddNewUserFriend(AddNewUserFriendRequest addNewUserFriendRequest)
         {
-            throw new NotImplementedException();
+            AddNewUserFriendResponse addNewUserFriendResponse = new AddNewUserFriendResponse();
+            try
+            {
+                User? user = await _context.Users.FirstOrDefaultAsync(u => u.Email == addNewUserFriendRequest.UserEmail);
+                if (user == null)
+                {
+                    addNewUserFriendResponse.IsSuccess = false;
+                    addNewUserFriendResponse.Message = "User not found";
+                }
+                else
+                {
+                    User? friend = await _context.Users.FirstOrDefaultAsync(u => u.FriendCode == addNewUserFriendRequest.FriendCode);
+                    if (friend == null)
+                    {
+                        addNewUserFriendResponse.IsSuccess = false;
+                        addNewUserFriendResponse.Message = "Friend not found";
+                    }
+                    else
+                    {
+                        if (!_context.UserFriends.Any(f => f.UserId == user.UserId && f.FriendId == friend.UserId))
+                        {
+                            addNewUserFriendResponse.IsSuccess = false;
+                            addNewUserFriendResponse.Message = "This user is already your friend";
+                        }
+                        else
+                        {
+                            UserFriend userFriend = new UserFriend
+                            {
+                                UserId = user.UserId,
+                                FriendId = friend.UserId,
+                            };
+
+                            _context.UserFriends.Add(userFriend);
+                            await _context.SaveChangesAsync();
+
+                            addNewUserFriendResponse.IsSuccess = true;
+                            addNewUserFriendResponse.FriendId = userFriend.FriendId;
+                            addNewUserFriendResponse.Message = "Friend added successfully";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                addNewUserFriendResponse.IsSuccess = false;
+                addNewUserFriendResponse.Message = $"Unexpected error on FriendRepository -> AddNewUserFriend: {ex.Message}";
+            }
+        
+            return addNewUserFriendResponse;
         }
 
         public async Task<DeleteFriendResponse> DeleteFriend(DeleteFriendRequest deleteFriendRequest)
