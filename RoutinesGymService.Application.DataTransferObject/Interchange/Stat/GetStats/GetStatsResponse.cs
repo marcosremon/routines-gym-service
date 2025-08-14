05ccabaa48aab05994f9c2ffb78f6ea5ba6f1742
marcos.remon <marcos.remon@kintech-engineering.com>
2025-08-14 14:37:37 +0200

    data trnsfero objects stats añadidos

---
+++ RoutinesGymService.Application.DataTransferObject/Interchange/Stat/GetStats/GetStatsResponse.cs
@@ -0,0 +1,9 @@
+﻿using RoutinesGymService.Transversal.Common;
+
+namespace RoutinesGymService.Application.DataTransferObject.Interchange.Stat.GetStats
+{
+    public class GetStatsResponse : BaseResponse
+    {
+        public List<RoutinesGymApp.Domain.Entities.Stat>? Stats { get; set; }
+    }
+}
