cfa4ff33290ab95c17c0919796424aa5e82dac1c
marcos.remon <marcos.remon@kintech-engineering.com>
2025-08-29 12:47:18 +0200

    ajuste para que deje revertir la lista de steps

--- RoutinesGymService.Transversal.JsonInterchange/Stat/GetDailyStepsInfo/GetDailyStepsInfoRequestJson.cs
+++ RoutinesGymService.Transversal.JsonInterchange/Stat/GetDailyStepsInfo/GetDailyStepsInfoRequestJson.cs
@@ -2,7 +2,7 @@
 {
     public class GetDailyStepsInfoRequestJson
     {
-        public string UserEmail { get; set; } = string.Empty;
+        public string? UserEmail { get; set; }
         public int? DailySteps { get; set; }
         public DateTime? Day { get; set; }
     }
