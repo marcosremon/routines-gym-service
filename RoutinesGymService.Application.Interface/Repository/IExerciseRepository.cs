fe8211deb67d85954b11da01f98e2cb761d33b67
marcos.remon <marcos.remon@kintech-engineering.com>
2025-09-09 08:28:56 +0200

    GetAllExerciseProgress hecho

--- RoutinesGymService.Application.Interface/Repository/IExerciseRepository.cs
+++ RoutinesGymService.Application.Interface/Repository/IExerciseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.AddExercise;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.AddExerciseProgress;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.DeleteExercise;
+using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.GetAllExerciseProgress;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.GetExercisesByDayAndRoutineId;
 using RoutinesGymService.Application.DataTransferObject.Interchange.Exercise.UpdateExercise;
 
@@ -11,6 +12,7 @@ namespace RoutinesGymService.Application.Interface.Repository
         Task<AddExerciseResponse> AddExercise(AddExerciseRequest addExerciseRequest);
         Task<AddExerciseAddExerciseProgressResponse> AddExerciseProgress(AddExerciseAddExerciseProgressRequest addExerciseRequest);
         Task<DeleteExerciseResponse> DeleteExercise(DeleteExerciseRequest deleteExerciseRequest);
+        Task<GetAllExerciseProgressResponse> GetAllExerciseProgress(GetAllExerciseProgressRequest getAllExerciseProgressRequest);
         Task<GetExercisesByDayAndRoutineNameResponse> GetExercisesByDayAndRoutineName(GetExercisesByDayAndRoutineNameRequest getExercisesByDayAndRoutineNameRequest);
         Task<UpdateExerciseResponse> UpdateExercise(UpdateExerciseRequest updateExerciseRequest);
     }
