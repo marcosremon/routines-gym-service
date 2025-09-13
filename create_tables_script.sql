83052d163a100991144bc581362700fb392231de
Marcos <mremonb@iesch.org>
2025-09-14 00:47:32 +0200

    generador de contraseña mas segura

--- create_tables_script.sql
+++ create_tables_script.sql
@@ -1,117 +1,117 @@
-CREATE TABLE users (
-    user_id BIGSERIAL PRIMARY KEY,
-    dni VARCHAR(255) NOT NULL DEFAULT '',
-    username VARCHAR(255) NOT NULL DEFAULT '',
-    surname VARCHAR(255) DEFAULT '',
-    email VARCHAR(255) NOT NULL DEFAULT '',
-    friend_code VARCHAR(255) NOT NULL DEFAULT '',
-    password BYTEA,
-    role INTEGER NOT NULL DEFAULT 0,
-    role_string VARCHAR(255) NOT NULL DEFAULT '',
-    inscription_date TIMESTAMP NOT NULL DEFAULT '0001-01-01 00:00:00'::timestamp
-);
+--CREATE TABLE users (
+--    user_id BIGSERIAL PRIMARY KEY,
+--    dni VARCHAR(255) NOT NULL DEFAULT '',
+--    username VARCHAR(255) NOT NULL DEFAULT '',
+--    surname VARCHAR(255) DEFAULT '',
+--    email VARCHAR(255) NOT NULL DEFAULT '',
+--    friend_code VARCHAR(255) NOT NULL DEFAULT '',
+--    password BYTEA,
+--    role INTEGER NOT NULL DEFAULT 0,
+--    role_string VARCHAR(255) NOT NULL DEFAULT '',
+--    inscription_date TIMESTAMP NOT NULL DEFAULT '0001-01-01 00:00:00'::timestamp
+--);
 
-CREATE TABLE user_friends (
-    user_friend_id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    friend_id BIGINT NOT NULL,
+--CREATE TABLE user_friends (
+--    user_friend_id BIGSERIAL PRIMARY KEY,
+--    user_id BIGINT NOT NULL,
+--    friend_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_user_friend_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(user_id),
+--    CONSTRAINT fk_user_friend_user
+--        FOREIGN KEY (user_id)
+--        REFERENCES users(user_id),
 
-    CONSTRAINT fk_user_friend_friends
-        FOREIGN KEY (friend_id)
-        REFERENCES users(user_id)
-);
+--    CONSTRAINT fk_user_friend_friends
+--        FOREIGN KEY (friend_id)
+--        REFERENCES users(user_id)
+--);
 
-CREATE TABLE steps (
-    steps_id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT '0001-01-01 00:00:00'::timestamp,
-    steps INTEGER NOT NULL DEFAULT 0,
-    daily_steps_goal INTEGER NOT NULL DEFAULT 10000,
+--CREATE TABLE steps (
+--    steps_id BIGSERIAL PRIMARY KEY,
+--    user_id BIGINT NOT NULL,
+--    date TIMESTAMP NOT NULL DEFAULT '0001-01-01 00:00:00'::timestamp,
+--    steps INTEGER NOT NULL DEFAULT 0,
+--    daily_steps_goal INTEGER NOT NULL DEFAULT 10000,
 
-    CONSTRAINT fk_steps_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(user_id)
-);
+--    CONSTRAINT fk_steps_user
+--        FOREIGN KEY (user_id)
+--        REFERENCES users(user_id)
+--);
 
-CREATE TABLE routines (
-    routine_id BIGSERIAL PRIMARY KEY,
-    routine_name VARCHAR(255) NOT NULL DEFAULT '',
-    routine_description VARCHAR(255) DEFAULT '',
-    user_id BIGINT NOT NULL,
+--CREATE TABLE routines (
+--    routine_id BIGSERIAL PRIMARY KEY,
+--    routine_name VARCHAR(255) NOT NULL DEFAULT '',
+--    routine_description VARCHAR(255) DEFAULT '',
+--    user_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_user_routine_routines
-        FOREIGN KEY (user_id)
-        REFERENCES users(user_id)
-);
+--    CONSTRAINT fk_user_routine_routines
+--        FOREIGN KEY (user_id)
+--        REFERENCES users(user_id)
+--);
 
-CREATE TABLE split_days (
-    split_day_id BIGSERIAL PRIMARY KEY,
-    day_name INTEGER NOT NULL,
-    day_name_string VARCHAR(255) NOT NULL,
-    routine_id BIGINT NOT NULL,
-    day_exercises_description TEXT NOT NULL DEFAULT '',
+--CREATE TABLE split_days (
+--    split_day_id BIGSERIAL PRIMARY KEY,
+--    day_name INTEGER NOT NULL,
+--    day_name_string VARCHAR(255) NOT NULL,
+--    routine_id BIGINT NOT NULL,
+--    day_exercises_description TEXT NOT NULL DEFAULT '',
 
-    CONSTRAINT fk_split_days_routine
-        FOREIGN KEY (routine_id)
-        REFERENCES routines(routine_id)
-);
+--    CONSTRAINT fk_split_days_routine
+--        FOREIGN KEY (routine_id)
+--        REFERENCES routines(routine_id)
+--);
 
-CREATE TABLE exercises (
-    exercise_id BIGSERIAL PRIMARY KEY,
-    exercise_name VARCHAR(255) NOT NULL DEFAULT '',
-    routine_id BIGINT NOT NULL,
-    split_day_id BIGINT NOT NULL,
+--CREATE TABLE exercises (
+--    exercise_id BIGSERIAL PRIMARY KEY,
+--    exercise_name VARCHAR(255) NOT NULL DEFAULT '',
+--    routine_id BIGINT NOT NULL,
+--    split_day_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_exercises_routine
-        FOREIGN KEY (routine_id)
-        REFERENCES routines(routine_id),
+--    CONSTRAINT fk_exercises_routine
+--        FOREIGN KEY (routine_id)
+--        REFERENCES routines(routine_id),
 
-    CONSTRAINT fk_exercises_split_day
-        FOREIGN KEY (split_day_id)
-        REFERENCES split_days(split_day_id)
-);
+--    CONSTRAINT fk_exercises_split_day
+--        FOREIGN KEY (split_day_id)
+--        REFERENCES split_days(split_day_id)
+--);
 
-CREATE TABLE exercise_progress (
-    progress_id BIGSERIAL PRIMARY KEY,
-    exercise_id BIGINT NOT NULL,
-    routine_id BIGINT NOT NULL,
-    day_name VARCHAR(50) NOT NULL DEFAULT '',
-    sets INTEGER NOT NULL,
-    reps INTEGER NOT NULL,
-    weight FLOAT NOT NULL,
-    performed_at TIMESTAMP NOT NULL,
+--CREATE TABLE exercise_progress (
+--    progress_id BIGSERIAL PRIMARY KEY,
+--    exercise_id BIGINT NOT NULL,
+--    routine_id BIGINT NOT NULL,
+--    day_name VARCHAR(50) NOT NULL DEFAULT '',
+--    sets INTEGER NOT NULL,
+--    reps INTEGER NOT NULL,
+--    weight FLOAT NOT NULL,
+--    performed_at TIMESTAMP NOT NULL,
 
-    CONSTRAINT fk_exercise_progress_exercise
-        FOREIGN KEY (exercise_id)
-        REFERENCES exercises(exercise_id),
+--    CONSTRAINT fk_exercise_progress_exercise
+--        FOREIGN KEY (exercise_id)
+--        REFERENCES exercises(exercise_id),
 
-    CONSTRAINT fk_exercise_progress_routine
-        FOREIGN KEY (routine_id)
-        REFERENCES routines(routine_id)
-);
+--    CONSTRAINT fk_exercise_progress_routine
+--        FOREIGN KEY (routine_id)
+--        REFERENCES routines(routine_id)
+--);
 
-INSERT INTO users (
-    dni,
-    username,
-    surname,
-    email,
-    friend_code,
-    password,
-    role,
-    role_string,
-    inscription_date
-) VALUES (
-    '00000000X',
-    'admin',
-    'Administrator',
-    'admin',
-    'ADMINCODE',
-    decode('EAimnd6R3iSAGdbmKabkkQ==', 'base64'),
-    1,
-    'ADMIN',
-    NOW()
-);
+--INSERT INTO users (
+--    dni,
+--    username,
+--    surname,
+--    email,
+--    friend_code,
+--    password,
+--    role,
+--    role_string,
+--    inscription_date
+--) VALUES (
+--    '00000000X',
+--    'admin',
+--    'Administrator',
+--    'admin',
+--    'ADMINCODE',
+--    decode('EAimnd6R3iSAGdbmKabkkQ==', 'base64'),
+--    1,
+--    'ADMIN',
+--    NOW()
+--);
