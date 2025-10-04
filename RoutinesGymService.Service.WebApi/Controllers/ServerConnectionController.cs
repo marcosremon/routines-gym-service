1906594860b5c4e577eb419685403ef8d79dc00d
Marcos <mremonb@iesch.org>
2025-10-04 17:06:29 +0200

    tonterias importantes

---
+++ RoutinesGymService.Service.WebApi/Controllers/ServerConnectionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutinesGymService.Service.WebApi.Controllers
+{
+    [ApiController]
+    public class ServerConnectionController : ControllerBase
+    {
+        #region CheckServerConnection
+        [HttpGet("CheckServerConnection")]
+        public ActionResult<string> CheckServerConnection()
+        {
+            return Ok("Ok");
+        }
+        #endregion
+    }
+}
