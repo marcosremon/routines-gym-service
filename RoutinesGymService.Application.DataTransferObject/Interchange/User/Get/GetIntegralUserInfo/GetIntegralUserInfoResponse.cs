0d9d0a1c5134ec181c8ec2b2c04c6d093240e16b
Marcos <mremonb@iesch.org>
2025-10-12 21:41:54 +0200

    password mejorado

---
+++ RoutinesGymService.Application.DataTransferObject/Interchange/User/Get/GetIntegralUserInfo/GetIntegralUserInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿using RoutinesGymService.Application.DataTransferObject.Entity;
+using RoutinesGymService.Transversal.Common.Responses;
+
+namespace RoutinesGymService.Application.DataTransferObject.Interchange.User.Get.GetIntegralUserInfo
+{
+    public class GetIntegralUserInfoResponse : BaseResponse
+    {
+        public UserDTO? UserDTO { get; set; }
+    }
+}
