4ab364f06b68ec0638569d03cd8fc67b760ea20d
marcos.remon <marcos.remon@kintech-engineering.com>
2025-10-14 17:08:42 +0200

    service status y database status bien hechos

--- RoutinesGymService.Service.WebApi/Controllers/ServerConnectionController.cs
+++ RoutinesGymService.Service.WebApi/Controllers/ServerConnectionController.cs
@@ -1,16 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RoutinesGymService.Infraestructure.Persistence.Context;
+using RoutinesGymService.Transversal.Common.Responses;
+using RoutinesGymService.Transversal.JsonInterchange.Status.CheckDatabaseConnection;
+using RoutinesGymService.Transversal.JsonInterchange.Status.CheckServiceConnection;
 
 namespace RoutinesGymService.Service.WebApi.Controllers
 {
     [ApiController]
-    [Route("check")]
+    [Route("status")]
     public class ServerConnectionController : ControllerBase
     {
-        #region CheckServerConnection
-        [HttpGet("CheckServerConnection")]
-        public ActionResult<string> CheckServerConnection()
+        private readonly ApplicationDbContext _context;
+
+        public ServerConnectionController(ApplicationDbContext context)
         {
-            return Ok("Ok");
+            _context = context;
+        }
+
+        #region CheckServiceConnection
+        [HttpGet("service")]
+        public ActionResult<CheckServiceConnectionResponseJson> CheckServiceConnection()
+        {
+            CheckServiceConnectionResponseJson checkServiceConnectionResponseJson = new CheckServiceConnectionResponseJson
+            {
+                CheckServiceConnection = "Serice connection Ok"
+            };
+
+            return Ok(checkServiceConnectionResponseJson);
+        }
+        #endregion
+
+        #region CheckDatabaseConnection
+        [HttpGet("database")]
+        public async Task<ActionResult<CheckDatabaseConnectionResponseJson>> CheckDatabaseConnection()
+        {
+            CheckDatabaseConnectionResponseJson checkDatabaseConnectionResponseJson = new CheckDatabaseConnectionResponseJson();
+            try
+            {
+                bool canConnect = await _context.Database.CanConnectAsync();
+                if (canConnect)
+                {
+                    checkDatabaseConnectionResponseJson.Status = ResponseCodesJson.OK;
+                    checkDatabaseConnectionResponseJson.DatabaseConnection = "Database connection Ok";
+                }
+                else
+                {
+                    checkDatabaseConnectionResponseJson.Status = ResponseCodesJson.INTERNAL_SERVER_ERROR;
+                    checkDatabaseConnectionResponseJson.DatabaseConnection = "Cannot connect to the database";
+                }
+            }
+            catch (Exception ex)
+            {
+                checkDatabaseConnectionResponseJson.Status = ResponseCodesJson.INTERNAL_SERVER_ERROR;
+                checkDatabaseConnectionResponseJson.DatabaseConnection = $"Database connection error {ex.Message}";
+            }
+
+            return Ok(checkDatabaseConnectionResponseJson);
         }
         #endregion
     }
---
+++ RoutinesGymService.Transversal.JsonInterchange/Status/CheckDatabaseConnection/CheckDatabaseConnectionResponseJson.cs
@@ -0,0 +1,10 @@
+﻿using RoutinesGymService.Transversal.Common.Responses;
+
+namespace RoutinesGymService.Transversal.JsonInterchange.Status.CheckDatabaseConnection
+{
+    public class CheckDatabaseConnectionResponseJson
+    {
+        public ResponseCodesJson Status { get; set; }
+        public string DatabaseConnection { get; set; } = string.Empty;
+    }
+}
