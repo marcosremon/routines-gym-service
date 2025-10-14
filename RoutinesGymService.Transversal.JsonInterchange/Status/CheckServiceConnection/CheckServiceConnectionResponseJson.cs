292fb0ef8f29ba6b019e3edc805ed27fc4c9b4d6
marcos.remon <marcos.remon@kintech-engineering.com>
2025-10-14 17:08:42 +0200

    service status y database status bien hechos

---
+++ RoutinesGymService.Transversal.JsonInterchange/Status/CheckServiceConnection/CheckServiceConnectionResponseJson.cs
@@ -0,0 +1,7 @@
+﻿namespace RoutinesGymService.Transversal.JsonInterchange.Status.CheckServiceConnection
+{
+    public class CheckServiceConnectionResponseJson
+    {
+        public string CheckServiceConnection { get; set; } = string.Empty;
+    }
+}
