a69c946f555c439fc50690dddb27f9d9872c91b2
marcos.remon <marcos.remon@kintech-engineering.com>
2025-10-29 17:02:25 +0100

    change password y refactor login

---
+++ RoutinesGymService.Application.DataTransferObject/Interchange/Admin/ChangeUserPassword/ChangeUserPasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutinesGymService.Application.DataTransferObject.Interchange.Admin.ChangeUserPassword
+{
+    public class ChangeUserPasswordRequest
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
