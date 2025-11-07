6417a71aaa2d4bd90b5e8952b66ece3d8d4c15ad
marcos.remon <marcos.remon@kintech-engineering.com>
2025-11-07 13:29:05 +0100

    faltaba un ;

---
+++ RoutinesGymService.Transversal.CleanSolution/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+
+namespace RoutinesGymService.Transversal.CleanSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We will delete all bin and obj folders in the solution before compile");
+
+            string location = Assembly.GetExecutingAssembly().Location;
+            string? solutionDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(location)).FullName).FullName).FullName).FullName;
+            Console.WriteLine("SolutionDirectory: " + solutionDirectory);
+
+            if (solutionDirectory != null)
+            {
+                List<string> proyects = new List<string>()
+                {
+                    "RoutinesGymService.Application.DataTransferObject",
+                    "RoutinesGymService.Application.Interface",
+                    "RoutinesGymService.Application.Mapper",
+                    "RoutinesGymService.Application.UseCase",
+
+                    "RoutinesGymService.Domain.Model",
+
+                    "RoutinesGymService.Infraestrucutre.Persistence",
+
+                    "RoutinesGymService.Service.WebApi",
+
+                    "RoutinesGymService.Transversal.Common",
+                    "RoutinesGymService.Transversal.JsonInterchange",
+                    "RoutinesGymService.Transversal.Security",
+                };
+
+                _DeleteBinFolders(solutionDirectory, proyects);
+                _DeleteObjFolders(solutionDirectory, proyects);
+
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+        }
+        private static void _DeleteBinFolders(string solutionDirectory, List<string> proyects)
+        {
+            foreach (string proyectFolder in proyects)
+            {
+                string binDirectory = Path.Combine(Path.Combine(solutionDirectory, proyectFolder), "bin");
+                Console.WriteLine("Deleted bin folders: {0}", binDirectory);
+
+                try
+                {
+                    if (Directory.Exists(binDirectory)) Directory.Delete(binDirectory, true);
+                }
+                catch { }
+            }
+        }
+
+        private static void _DeleteObjFolders(string solutionDirectory, List<string> proyects)
+        {
+            foreach (string proyectFolder in proyects)
+            {
+                string objDirectory = Path.Combine(Path.Combine(solutionDirectory, proyectFolder), "obj");
+                Console.WriteLine("Deleted obj folders: {0}", objDirectory);
+
+                try
+                {
+                    if (Directory.Exists(objDirectory)) Directory.Delete(objDirectory, true);
+                }
+                catch { }
+            }
+        }
+    }
+}
